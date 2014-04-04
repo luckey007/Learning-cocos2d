e9a98a7bc555a3829716aa7f99681de3cff606e2
luckey007 <lakhwani.lucky@gmail,com>
2014-04-04 11:32:01 +0530

    bom

--- CutCut/CutCut/PolygonSprite.mm
+++ CutCut/CutCut/PolygonSprite.mm
@@ -1,10 +1,4 @@
-//
-//  PolygonSprite.m
-//  CutCutCut
-//
-//  Created by Allen Benson G Tan on 5/20/12.
-//  Copyright 2012 WhiteWidget Inc. All rights reserved.
-//
+
 
 #import "PolygonSprite.h"
 
--- CutCut/Strawberry.h
+++ CutCut/Strawberry.h
@@ -1,9 +1,4 @@
-//
-//  Strawberry.h
-//  CutCutCut
-//
-//  Created by Allen Benson G Tan on 5/16/12.
-//  Copyright 2012 WhiteWidget Inc. All rights reserved.
+
 //
 
 #import <Foundation/Foundation.h>
